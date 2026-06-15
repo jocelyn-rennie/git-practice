60cef3d1c688229c345a7480e4c04741c5710307
jocelyn-rennie <jocelyn.rennie.uk@gmail.com>
2026-06-15 17:52:22 +0100

    for loop

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        /*
         System.out.println("Hello from B");
         System.out.println("Test1");
 
         int i=0;
 
         while (i<10){
-            System.out.println(i);
+            System.out.print(i);
+            if (i%2==0){
+                System.out.println(" even");
+            }
+            else{
+                System.out.println(" odd");
+            }
             i++;
+        }
+
+         */
+
+        for( int i =0; i<10; i++){
+            System.out.println(i);
         }
 
     }
